e3d3a671a1e64304e9802417e650c26f835af60e
handokowic <handoko_d_wicaksono@telkomsel.co.id>
2026-01-22 10:20:01 +0700

    change wording

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getFreeText(freetext: string): string {
-    return freetext;
+    return `text inputted : ${freetext}`;
   }
   getHello(): string {
     return 'Test Deployment';
